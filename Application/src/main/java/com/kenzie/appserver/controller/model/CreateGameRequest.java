d794c78ab28e1e1c93fbcad5b0ea9a7104ed098e
Aaron Hoyt <aaron.hoyt@mykenzie.snhu.edu>
2023-06-10 15:34:56 -0500

    new changes

--- Application/src/main/java/com/kenzie/appserver/controller/model/CreateGameRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/CreateGameRequest.java
@@ -1,6 +1,7 @@
 package com.kenzie.appserver.controller.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Optional;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -17,6 +18,16 @@ public class CreateGameRequest {
     @NotEmpty
     @JsonProperty("isAvailable")
     private boolean isAvailable;
+
+    private Optional<String> borrower;
+
+    public Optional<String> getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Optional<String> borrower) {
+        this.borrower = borrower;
+    }
 
     public CreateGameRequest(String title, String loaner, boolean isAvailable) {
         this.title = title;
